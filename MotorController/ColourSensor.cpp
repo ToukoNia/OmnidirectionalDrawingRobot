03019b8330461b33afe7490e4406c80f6f6ec851
ToukoNia <88745518+ToukoNia@users.noreply.github.com>
2025-02-21 03:01:49 +0000

    Update ColourSensor.cpp

--- MotorController/ColourSensor.cpp
+++ MotorController/ColourSensor.cpp
@@ -14,7 +14,7 @@ void ColourSensorArray::readSensors(int colour){
       }
   }
   j=0;
-}
+} //plan to extend this for case off the line: add flag j=2 for 2 found. if j!=2, then we return 1 from function (otherwise 0). Make new function that does the opposite of the main function to cawlculate opposite vector.
 
 void ColourSensorArray::assignAngle(){
   x=sensorXDistance[sensors[0]]-sensorXDistance[sensors[1]];  //favours left side
@@ -26,7 +26,7 @@ void ColourSensorArray::assignAngle(){
   lastValue=sensors[0]; //will always be the first sensor as we know the robot will always be heading forwards
 }
 
-void ColourSensorArray::normalise(){
+void ColourSensorArray::normalise(){  //normalises the vector so the system total speed nvr goes over the system speed
   temp=sqrt(sq(x)+sq(y));
   x=x/i;
   y=y/i;
