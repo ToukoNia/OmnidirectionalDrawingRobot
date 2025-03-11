af042383c04b407dd966ede8529858e59e76422e
ToukoNia <88745518+ToukoNia@users.noreply.github.com>
2025-03-11 19:27:59 +0000

    Fixes from the other branch stuff

--- MotorController/body.cpp
+++ MotorController/body.cpp
@@ -5,24 +5,31 @@ void RobotBody::Setup(){
 }
 
 void RobotBody::StartSequence(){
-  findWall(20,16,0,1,1);  //endponint in cm, how far from wall in cm, if sideways is now forwards, if forwards is now sideways, direction
-  base.Rotate(100);
+  
+  findDistance(0);
+  findWall(19,16,0,1,1);  //endponint in cm, how far from wall in cm, if sideways is now forwards, if forwards is now sideways, direction
+  base.Rotate(95);
   delay(100);
-  findWall(17,20,0,1,1);
+  findDistance(1);
+  findWall(8,13,0,1,1);
+
 }
 
 
 void RobotBody::FinalStretch(){
-  delay(20);
-  findWall(83,15,1,0,0);
-  findWall(80,83,0,1,0);
+  base.calculateTrajectory(-1,0);
+  delay(300);
+  findWall(lengths[0]+1,12,1,0,0);
+  findWall(lengths[1],lengths[0]+1,0,1,0);
 }
 
-
+void RobotBody::findDistance(bool direction){
+  lengths[direction]=Ultrasonics[0].convert_cm(Ultrasonics[0].ping_median(5));
+}
 
 void RobotBody::staticDistance(int targetDistance, bool sideDirection,int robotDirection){
-  distance=Ultrasonics[sideDirection].ping_cm();
-    if ((distance<=0.8*targetDistance || distance>=1.2*targetDistance)&&distance){  //have a way to flip which trajectory mayb?
+  distance=Ultrasonics[sideDirection].convert_cm(Ultrasonics[sideDirection].ping_median(3));
+    if ((distance<=targetDistance+DISTANCE_BUFFER || distance>=DISTANCE_BUFFER+targetDistance)&&distance){  //have a way to flip which trajectory mayb?
       if (sideDirection){
         base.calculateTrajectory(robotDirection,0.75*(targetDistance-distance)/(targetDistance)); //might need to flip this if vertical and horizontal gets flipped, or add a proportional control
       } else {
@@ -40,11 +47,11 @@ void RobotBody::staticDistance(int targetDistance, bool sideDirection,int robotD
 
 void RobotBody::findWall(int targetDistance, int constantDistance, bool forwardDirection, bool sideDirection, bool robotOrientation){
   if (robotOrientation){
-    while(Ultrasonics[forwardDirection].convert_cm(Ultrasonics[forwardDirection].ping_median(3))>targetDistance){
+    while(Ultrasonics[forwardDirection].convert_cm(Ultrasonics[forwardDirection].ping_median(5))>targetDistance){
       staticDistance(constantDistance,sideDirection,1);
    }
   } else {
-    while(Ultrasonics[forwardDirection].convert_cm(Ultrasonics[forwardDirection].ping_median(3))<targetDistance){
+    while(Ultrasonics[forwardDirection].convert_cm(Ultrasonics[forwardDirection].ping_median(5))<targetDistance){
       staticDistance(constantDistance,sideDirection,-1);
    }
   }
--- MotorController/body.h
+++ MotorController/body.h
@@ -4,16 +4,19 @@
 #include "OmnidirectionalController.h"
 #include <Arduino.h>
 #include <NewPing.h>
+#define DISTANCE_BUFFER 2
+#define K 0.75
 
 class RobotBody { 
   private:
     NewPing Ultrasonics[2]={NewPing(25,27),NewPing(29,31)};  //0 is in line with x axis, 1 is in line with the y axis on the robot
     OmnidirectionalController base;
-    int distance;
+    int distance; int lengths[2];
   public:
     void StartSequence();
     void FinalStretch();
     void Setup();
+    void findDistance(bool direction);
     void staticDistance(int targetDistance, bool sideDirection, int robotDirection);
     void findWall(int targetDistance, int staticDistance, bool forwardDirection, bool sideDirection, bool robotOrientation);
 };
